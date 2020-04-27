dce6db95615259be3fb06506ddc1d5f70256556e
akshataw <wagheakshu@gmail.com>
2020-04-27 17:00:28 +0530

    add food functionality

---
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.dinnerdecider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "Mc Donalds", "Joyes Pizza")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decide_button.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+
+            selectedFoodText.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+            addFoodText.text.clear()
+            println(foodList)
+        }
+
+    }
+}
